1b3ba7f3902d51572421cb3656517e82367cc17d
Joel Trinidad <jth_2929@hotmail.com>
2022-07-30 19:10:37 -0500

    show information

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,10 +7,37 @@ export default function Characters(props) {
         <div className="container-characters">
             { characters.map((character, index) => (
                 <div className="character-container" key={index}>
-                    <p>{character.name}</p>
+                    <div>
+                        <img src={character.image} alt={character.name} />
+                    </div>
+                    <div>
+                        <h3>{character.name}</h3>
+                        <h6>
+                            {character.status === "Alive" ? (
+                                <>
+                                    <span className="alive"></span>
+                                    Alive
+                                </>
+                            ) : (
+                                <>
+                                    <span className="dead"></span>
+                                    dead
+                                </>
+                            )}
+                        </h6>
+                        <p>
+                            <span className="text-grey">Episodios: </span>
+                            <span>{character.episode.length}</span>
+                        </p>
+                        <p>
+                            <span className="text-grey">Especie: </span>
+                            <span>{character.species}</span>
+                        </p>
+                    </div>
                 </div>
             ))}
         </div>
+        <span className="back-home">Volver al home</span>
     </div>
     )
 }
